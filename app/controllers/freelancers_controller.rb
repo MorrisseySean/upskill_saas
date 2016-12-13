2a7f4d7dbb52881185de2fe164885857de755561
Sean Morrissey <morroclearwater@hotmail.com>
2016-12-13 05:01:42 +0000

    Added scraped freelancer info to freelancer pages

--- app/controllers/freelancers_controller.rb
+++ app/controllers/freelancers_controller.rb
@@ -14,7 +14,7 @@ protected
         @char_data = Hash.new()
         
         # Scrape images from the page
-        @char_data[:ability_img] = "//webcdn.triongames.com/ar/assets/images/char/abilities/abilities_lockwood.png"
+        @char_data[:ability_img] = "//webcdn.triongames.com/ar/assets/images/char/abilities/abilities_#{params[:id]}.png"
         @char_data[:full_img] = doc.css('.bg-mobile-container').to_s.sub(/(.*)(?:url\()/, "").sub(/\);(.*)/, "")
         
         # Scrape character details
@@ -22,6 +22,8 @@ protected
         @char_data[:desc] = doc.css('.char-desc').text
         @char_data[:role] = doc.css('.char-bio').first.text.capitalize
         @char_data[:faction] = doc.css('.char-bio').last.text.capitalize
+        dossier = doc.css('.dossier-content')
+        @char_data[:full_desc] = dossier.at_css('.description').text.sub(/(\(\d\))/, "")
         
         # Scrape character ability data 
         ability_info = doc.css('.character-ability-info')
@@ -32,6 +34,16 @@ protected
             # Ability name data
             @char_data[:ability][x] = Hash.new()
             @char_data[:ability][x][:name] = ab_info.at_css('h3').text
+            @char_data[:ability][x][:description] = ab_info.at_css('.ability-desc').text
+            
+        end
+        
+        # Scrape character ability videos
+        x = 0
+        ability_videos = doc.css('.character-ability-video');
+        ability_videos.each do |vid|
+            x += 1
+            @char_data[:ability][x][:video] = vid.at_css('source').to_s.sub(/(.*)(?:src=")/, "").sub(/(?:"><\/source)(.*)/, "")
         end
         
     end
