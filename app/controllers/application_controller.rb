7f73407a4d47dd7cd244e96b6a02f1e251285bd7
Sean Morrissey <morroclearwater@hotmail.com>
2017-04-24 20:02:00 +0000

    Attempted fix for freelancer bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
   
   # Get a list of freelancer names
   before_filter :get_chars
+  
     
   protected
     def configure_permitted_parameters
