f020e5455241cb5d2990c0266b1f2a942119d2cd
Sean Morrissey <morroclearwater@hotmail.com>
2017-05-19 20:31:34 +0000

    Match page rough draft added

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -11,8 +11,24 @@ class MatchesController < ApplicationController
         redirect_to event_path(params[:match][:event_id])
     end
     
+    def show
+        @match = Match.find(params[:id])
+        @home_team = Team.find(@match.home_team)
+        @away_team = Team.find(@match.away_team)
+        @home_captain = false
+        @away_captain = false
+        # Check if the current user is captain of either team
+        if user_signed_in?
+            if current_user.id == @home_team.user_id
+                @home_captain = true
+            elsif current_user.id == @away_team.user_id
+                @away_captain = true
+            end
+        end
+    end
+    
 private
     def match_params
-        params.require(:match).permit(:rounds, :home_team, :away_team, :event_id)
+        params.require(:match).permit(:round, :home_team, :away_team, :event_id)
     end
 end
