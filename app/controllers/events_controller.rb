868e3049bbfe7b73aec6625399cd14dde23ec542
Sean Morrissey <morroclearwater@hotmail.com>
2017-05-19 17:08:07 +0000

    Teams can be removed from events by team captains or admins

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,7 +9,7 @@ class EventsController < ApplicationController
               if current_user.profile.team_id > 0
                   team = Team.find(current_user.profile.team_id)
                   if current_user.id == team.user_id
-                      if team.profiles.count >= 4
+                      if team.profiles.count >= 0
                           @captain = true
                           @message = Message.new()
                       end
@@ -27,15 +27,27 @@ class EventsController < ApplicationController
     end
     
     def update
+      # Get the event, list of events and subject team
       @event = Event.find(params[:id])
       @new_team = Team.find(params[:event][:team_id])
       @event_teams = @event.teams
-      if @event_teams.include?@new_team
-        flash[:danger] = "Your team is already signed up for " + @event.name + "!"
-      else
-        @event.teams << @new_team
-        flash[:success] = "Your team has signed up for " + @event.name + "!"
+      
+      # If the user is trying to remove a team from the league
+      if params[:event][:msg] == "leave"
+        # Check the team is already part of the event
+        if @event_teams.include?@new_team
+          # Remove the team and output a message to the user
+          @event.teams.delete(@new_team)
+          flash[:danger] = @new_team.name + " has left " + @event.name
+        end
+      elsif params[:event][:msg] == "join"
+        if @event_teams.include?@new_team
+          flash[:danger] = "Your team is already signed up for " + @event.name + "!"
+        else
+          @event.teams << @new_team
+          flash[:success] = "Your team has signed up for " + @event.name + "!"
+        end
       end
-      redirect_to team_path( params[:event][:team_id] )
+      redirect_to event_path( @event.id )
     end
 end
